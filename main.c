b2a9850bfc08ccf9d29ccbaf7f311ca4d638943d
juliane-meyer <juli.mey@outlook.de>
2022-05-24 11:13:33 +0200

    gitignordatei hinzugefuegt

--- main.c
+++ main.c
@@ -1,103 +1,95 @@
-/**
- *  Author: Juliane Meyer
- *  Datum: 15.05.2022
- *  Programm: Tag des Jahres in mehreren Funktionen
- **/
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-static int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
 /*
-Hier wird geprüft, ob ein Schaltjahr vorliegt.
+Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des gegorianischen Kalender ein Schaltjahr
+ist.
 */
 int is_leapyear(int year){
-    if(year<1582) return -1;
-    if(year%4==0){
-        if(year%100==0){
-            if(year%400==0){
+    if(year % 4 == 0){
+        if(year % 100 == 0){
+            if(year % 400 == 0){
                 return 1;
             }
            return 0;
         }
         return 1;
     }
-    return -1;
+    return 0;
 }
 
 /*
-Hier wird der Tag, der Monat und das Jahr eingelesen. Wenn das angegebene Datum
-ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
+Die Funktion bestimmt für einen gegebenen Monat, eines gegebenen Jahres, wie viele Tage der Monat hat. Der
+Wert des Monats muss zwischen 1 und 12 liegen. Schaltjahre werden berücksichtigt.
 */
-void input_date(int *day, int *month, int *year){
-    char datum[10];
-    printf(" *** Tag des Jahres *** \n");
-    printf("Bitte geben Sie ein Datum ein: ");
-    scanf("%10s",datum);
-    char *token=strtok(datum,".");
-    *day=atoi(token);
-    token=strtok(NULL,".");
-    *month=atoi(token);
-    token=strtok(NULL,".");
-    *year=atoi(token);
+
+int get_days_for_month(int year, int month){
+    if(month > 12 || month < 1){
+        return -1;
+    }
+    int days_of_months[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if(is_leapyear(year) && month == 2){
+            return 29;
+        }
+    return days_of_months[month-1];
 }
 
 /*
-Hier wird bestimmt, wie viel Tage der eingegebene Monat hat.
-*/
-int get_days_for_month(int month, int year){
-    tage_pro_monat[1]=28;
-    if((month<1 || month>12) || (year<1582 || year>2400)) return -1;
-    if(is_leapyear(year)) tage_pro_monat[1]++;
-    return tage_pro_monat[month];
-}
-
-
-/*
-Das Daten wir hier überprüft. Ungültig ist das Datum vor dem 1.1.1582 und alles nach dem 31.12.2400.
+Die Funktion überprüft, ob ein eingegebenes Datum gültig ist. Daten vor dem 1.1.1582 sind ungültig, genauso
+wie alle Daten nach dem 31.12.2400.
 */
 int exists_date(int day, int month, int year){
-    if (year<1582 || year>2400) return 0;
-    if(month<1 || month>12) return 0;
-    if(tage_pro_monat[month-1]<day || day<1) return 0;
+    if(year < 1582 || month < 1 || day < 1 || year > 2400 || month > 12 || day > 31){
+        return 0;
+    }
+    if(get_days_for_month(year, month) < day) {
+        return 0;
+    }
     return 1;
 }
 
-
 /*
-Hier wird die Die Funktion berechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag, Monat
+Die Funktion liest 3 Ganzzahlwerte (Integer) ein, für Tag, Monat und Jahr. Wenn das angegebene Datum
+ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
+*/
+
+int input_date(int *day, int *month, int *year){
+    do{
+        printf("Geben Sie ein Jahr ein: ");
+        scanf("%d", year);
+        printf("Geben Sie einen Monat ein: ");
+        scanf("%d", month);
+        printf("Geben Sie einen Tag ein: ");
+        scanf("%d", day);
+    } while (exists_date(*day, *month, *year) != 1);
+}
+
+/* Funktion um herauszufinden, welcher Tag im Jahr das ist.
+Die Funktion berechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag, Monat
 und Jahr die Nummer des Tages, gezählt von Jahresbeginn (1. Januar) an. Schaltjahre werden bei der
 Berechnung berücksichtigt. Ist das übergebene Datum ungültig, beträgt der Rückgabewert -1.
 */
 int day_of_the_year(int day, int month, int year){
-    if(!exists_date(day,month,year)) return -1;
-    int erg=0;
-    if(is_leapyear(year)){
-        tage_pro_monat[1]++;
-    }
-    if(month==1){
-        return day;
+    if(exists_date(day, month, year) != 1){
+        return -1;
     }
 
-    for (int i = 0; i <= month-1; i++) {
-        erg+=get_days_for_month(i,year);
+    int result = 0;
+
+    for (int i = 1; i < month; i++) {
+        result += get_days_for_month(year, i);
     }
-    if(is_leapyear(year)) tage_pro_monat[1]--;
-    erg+=day;
-    return erg;
+    result += day;
+
+    return result;
 }
 
-/*
-Hauptfunktion: Hier werden die Funktionen aufgerufen und die Ausgabe der Zahl des Tages erfolgt.
-*/
+// Hauptfunktion
 int main()
 {
-    int year,month,day=0;
-    input_date(&day,&month,&year);
-    printf("Das Datum ist der %d. Tag des Jahres %d",day_of_the_year(day,month,year),year);
+    int day,month,year;
 
+    input_date(&day, &month, &year);
 
+    printf(" *** Tag des Jahres *** \n");
+
+    printf("Der %d.%d.%d Es ist der %d. Tag des Jahres.",day, month, year, day_of_the_year(day, month, year));
     return 0;
 }
